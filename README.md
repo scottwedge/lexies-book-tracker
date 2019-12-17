a082e39108673f54cd83f8a61ece0e139aa26894
Alex Chan <alex@alexwlchan.net>
2019-12-17 08:47:19 +0000

    Start showing a list of reviews, think about the data model

---
+++ README.md
@@ -0,0 +1,13 @@
+# read
+
+just the faves
+show everything
+
+# reading
+
+# to read
+
+i own a copy
+recommended to me
+recommended by a friend
+looks interesting
--- src/routes.py
+++ src/routes.py
@@ -1,9 +1,19 @@
 # -*- encoding: utf-8
 
+from flask import render_template
+
 from src import app
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello, World!"
+    return render_template(
+        "reviews.html",
+        name="lexie",
+        reviews=[
+            "This book was good.",
+            "This book was also good.",
+            "This book was bad.",
+        ]
+    )
