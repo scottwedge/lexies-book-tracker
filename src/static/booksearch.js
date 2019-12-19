e9240ff69f7c3ed43d3d010178b51200cb252b42
Alex Chan <alex@alexwlchan.net>
2019-12-19 23:41:11 +0000

    Sort out the reviews

--- src/static/booksearch.js
+++ src/static/booksearch.js
@@ -123,3 +123,43 @@ class Booksearch {
     }
   }
 }
+
+class Books {
+  constructor(container) {
+    this.container = container;
+    this.editing = null;
+  }
+
+  addEventListeners() {
+    [...document.querySelectorAll(".edit-book")].forEach(item => {
+      item.addEventListener("click", evt => {
+        if (this.editing === null) {
+          this.editing = parseInt(evt.target.dataset["bookid"]);
+        } else {
+          this.editing = null;
+        }
+        this.renderEditing();
+      });
+    });
+    [...document.querySelectorAll(".cancel-edit")].forEach(item => {
+      item.addEventListener("click", evt => {
+        this.editing = null;
+        this.renderEditing();
+      });
+    });
+  }
+
+  renderEditing() {
+    const div = document.getElementById("book-" + this.editing);
+
+    if (this.editing) {
+      const form = div.querySelector(".book-edit-form");
+
+      form.classList.remove("hidden");
+    } else {
+      [...document.querySelectorAll(".book-edit-form")].forEach(item => {
+        item.classList.add("hidden");
+      });
+    }
+  }
+}
