90066435bdd7bf151e66b89ed87801bc7dcfbbf7
Alex Chan <alex@alexwlchan.net>
2019-12-22 18:39:22 +0000

    Add a test that curly quotes render correctly on the plan page

--- tests/test_smartypants.py
+++ tests/test_smartypants.py
@@ -14,3 +14,24 @@ def test_reading_page_title_is_curly_quotes(client, user):
     soup = bs4.BeautifulSoup(resp.data, "html.parser")
 
     assert soup.find("title").text == "what i’m reading 📚 lexie’s book tracker"
+
+
+def test_plan_page_is_curly_quotes(client, book, user):
+    book_title = "Old Possum’s Book of Practical Cats"
+    note_text = "This is a book I haven’t read yet"
+
+    book.title = book_title
+
+    plan = Plan.create(
+        note=note_text,
+        book=book,
+        user=user
+    )
+
+    resp = client.get("/to-read")
+    soup = bs4.BeautifulSoup(resp.data, "html.parser")
+
+    book_div = soup.find("div", attrs={"class": "book"})
+
+    assert book_div.find("h3", attrs={"class": "book-title"}).text.strip() == book_title
+    assert book_div.find("div", attrs={"class": "plan-note"}).text.strip() == note_text
