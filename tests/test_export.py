cc873d51b16370dfb64add7c4b6c49405f95398a
Alex Chan <alex@alexwlchan.net>
2020-01-08 08:04:44 +0000

    Check the "download spreadsheet link" is present

--- tests/test_export.py
+++ tests/test_export.py
@@ -2,6 +2,8 @@ import csv
 import datetime
 import io
 
+import bs4
+
 from helpers import create_book
 from src.models import Plan, Reading, Review
 
@@ -10,7 +12,22 @@ def today():
     return datetime.datetime.now().date()
 
 
-def test_can_export_reviews(client, session, fake, logged_in_user):
+def _find_download_link(client, path):
+    resp = client.get(path)
+
+    soup = bs4.BeautifulSoup(resp.data, "html.parser")
+
+    a_tags = soup.find_all("a")
+
+    download_links = [a_t for a_t in a_tags if a_t.text == "download as a spreadsheet"]
+
+    try:
+        return download_links[0].attrs["href"]
+    except IndexError:
+        return
+
+
+def test_can_export_reviews(client, session, fake, user):
     book1 = create_book(
         session=session, fake=fake, isbn_10=fake.numerify(), isbn_13=fake.numerify()
     )
@@ -18,14 +35,17 @@ def test_can_export_reviews(client, session, fake, logged_in_user):
         session=session, fake=fake, isbn_10=fake.numerify(), isbn_13=fake.numerify()
     )
 
-    review1 = Review(review_text=fake.text(), book=book1, user=logged_in_user)
-    review2 = Review(review_text=fake.text(), book=book2, user=logged_in_user)
+    review1 = Review(review_text=fake.text(), book=book1, user=user)
+    review2 = Review(review_text=fake.text(), book=book2, user=user)
 
     session.add(review1)
     session.add(review2)
     session.commit()
 
-    resp = client.get("/export/reviews")
+    download_link = _find_download_link(client, path="/read")
+    assert download_link is not None
+
+    resp = client.get(download_link)
     csv_buf = io.StringIO(resp.data.decode("utf-8"))
 
     csv_rows = [dict(row) for row in csv.DictReader(csv_buf)]
@@ -58,10 +78,10 @@ def test_can_export_reviews(client, session, fake, logged_in_user):
     ]
 
 
-def test_empty_isbn_is_empty_string_in_csv(client, session, fake, book, logged_in_user):
+def test_empty_isbn_is_empty_string_in_csv(client, session, fake, book, user):
     book.isbn_10 = ""
     book.isbn_13 = ""
-    review = Review(review_text=fake.text(), book=book, user=logged_in_user)
+    review = Review(review_text=fake.text(), book=book, user=user)
 
     session.add(review)
     session.commit()
@@ -76,12 +96,10 @@ def test_empty_isbn_is_empty_string_in_csv(client, session, fake, book, logged_i
     assert row["isbn_13"] == ""
 
 
-def test_review_date_is_isoformat(client, session, fake, book, logged_in_user):
+def test_review_date_is_isoformat(client, session, fake, book, user):
     book.isbn_10 = ""
     book.isbn_13 = ""
-    review = Review(
-        review_text=fake.text(), date_read=today(), book=book, user=logged_in_user,
-    )
+    review = Review(review_text=fake.text(), date_read=today(), book=book, user=user)
 
     session.add(review)
     session.commit()
@@ -95,7 +113,7 @@ def test_review_date_is_isoformat(client, session, fake, book, logged_in_user):
     assert row["date_read"] == today().strftime("%Y-%m-%d")
 
 
-def test_can_export_plans(client, session, fake, logged_in_user):
+def test_can_export_plans(client, session, fake, user):
     book1 = create_book(
         session=session, fake=fake, isbn_10=fake.numerify(), isbn_13=fake.numerify()
     )
@@ -103,14 +121,17 @@ def test_can_export_plans(client, session, fake, logged_in_user):
         session=session, fake=fake, isbn_10=fake.numerify(), isbn_13=fake.numerify()
     )
 
-    plan1 = Plan(note=fake.text(), book=book1, user=logged_in_user, date_added=today())
-    plan2 = Plan(note=fake.text(), book=book2, user=logged_in_user, date_added=today())
+    plan1 = Plan(note=fake.text(), book=book1, user=user, date_added=today())
+    plan2 = Plan(note=fake.text(), book=book2, user=user, date_added=today())
 
     session.add(plan1)
     session.add(plan2)
     session.commit()
 
-    resp = client.get("/export/plans")
+    download_link = _find_download_link(client, path="/to-read")
+    assert download_link is not None
+
+    resp = client.get(download_link)
     csv_buf = io.StringIO(resp.data.decode("utf-8"))
 
     csv_rows = [dict(row) for row in csv.DictReader(csv_buf)]
@@ -162,7 +183,10 @@ def test_can_export_reading(client, session, fake, logged_in_user):
     session.add(reading2)
     session.commit()
 
-    resp = client.get("/export/reading")
+    download_link = _find_download_link(client, path="/reading")
+    assert download_link is not None
+
+    resp = client.get(download_link)
     csv_buf = io.StringIO(resp.data.decode("utf-8"))
 
     csv_rows = [dict(row) for row in csv.DictReader(csv_buf)]
